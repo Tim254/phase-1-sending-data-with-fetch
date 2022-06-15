473a691ae9fdc0d44b459a4bf96ebdabd45f1e88
Tim254 <tim.mailu@gmail.com>
2022-06-16 00:46:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const submitData = (name , email) => {
+const userInfo = {
+    name: name,
+    email: email,
+};
+
+
+// const configObject = 
+
+
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(userInfo)
+})
+.then(function(response) {
+    response.json()
+})
+.then(function (object){
+    document.body.innerHTML = object.id
+})
+.catch(err => {
+        
+    document.body.innerHTML = err.message
+})
+}
