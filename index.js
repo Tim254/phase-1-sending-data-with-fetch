e16c482c9f618d9ab28e1155e12700fb8816c14d
Tim254 <tim.mailu@gmail.com>
2022-06-16 00:50:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-// Add your code here
-const submitData = (name , email) => {
-const userInfo = {
-    name: name,
-    email: email,
-};
+// // Add your code here
+// const submitData = (name , email) => {
+// const userInfo = {
+//     name: name,
+//     email: email,
+// };
 
 
-// const configObject = 
+// // const configObject = 
 
 
-return fetch("http://localhost:3000/users", {
-    method: "POST",
-    headers: {
-        "content-Type": "application/json",
-        Accept: "application/json",
-    },
-    body: JSON.stringify(userInfo)
-})
-.then(function(response) {
-    response.json()
-})
-.then(function (object){
-    document.body.innerHTML = object.id
-})
-.catch(err => {
+// return fetch("http://localhost:3000/users", {
+//     method: "POST",
+//     headers: {
+//         "content-Type": "application/json",
+//         Accept: "application/json",
+//     },
+//     body: JSON.stringify(userInfo)
+// })
+// .then(function(response) {
+//     response.json()
+// })
+// .then(function (object){
+//     document.body.innerHTML = object.id
+// })
+// .catch(err => {
         
-    document.body.innerHTML = err.message
-})
+//     document.body.innerHTML = err.message
+// })
+// }
+const submitData = (name, email) => {
+    const userInfo = {name: name, email:email};
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(err => {
+        
+        document.body.innerHTML = err.message
+    })
 }
